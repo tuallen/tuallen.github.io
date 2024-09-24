766aad1da4779e76b4a1af8bbe945ab6472dd3e9
Allen Tu <allentu3@gmail.com>
2024-09-24 11:50:23 -0400

    Predict and Aggregate

--- static/js/video-zoom.js
+++ static/js/video-zoom.js
@@ -8,3 +8,13 @@ document.querySelectorAll('.video-container').forEach(container => {
         video.currentTime = 0;  // Reset to the beginning when hover ends
     });
 });
+
+document.querySelectorAll('.image-container').forEach(container => {
+    const image = container.querySelector('.image-thumbnail');
+    container.addEventListener('mouseover', () => {
+        image.style.transform = 'scale(3)'; // Zoom the image on hover
+    });
+    container.addEventListener('mouseleave', () => {
+        image.style.transform = 'scale(1)'; // Reset the zoom when hover ends
+    });
+});
